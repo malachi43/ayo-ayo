c46999023bb22f7844427b4468af5f1a8bbd6376
Malachi <ukomalachi@gmail.com>
2024-05-14 03:09:10 +0100

    add ayo game logic

--- server/src/index.js
+++ server/src/index.js
@@ -29,6 +29,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static("public"));
 
 app.post(`${baseUrl}/login`, authController.login);
 app.post(`${baseUrl}/register`, authController.register);
