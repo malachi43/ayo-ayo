1e4e3e63c9727ba157f2117cbb9a74861819ab4f
Malachi <ukomalachi@gmail.com>
2024-05-23 23:26:33 +0100

    convert Mongoose.ObjectId in avatar model to string

--- server/src/models/avatar.js
+++ server/src/models/avatar.js
@@ -2,10 +2,12 @@ const { Schema, model } = require("mongoose");
 
 const avatarSchema = new Schema({
     avatarList: {
-        type: [{ avatar_url: String}],
+        type: [{ avatar_url: String }],
         required: true,
         default: []
     }
 })
+
+avatarSchema.path("avatarList._id").get(v => v.toString())
 
 module.exports = model("avatarList", avatarSchema);
